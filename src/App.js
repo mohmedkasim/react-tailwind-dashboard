505c31ac7f7f2fec93b64fc3f1ef3a1df3a7ea1c
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-02-07 17:48:45 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import "./App.css";
 const App = () => {
   return (
-    <div>App</div>
+  <h1 className="underline text-3xl text-clip">App2</h1>
   )
-}
+};
 
-export default App
+export default App;
