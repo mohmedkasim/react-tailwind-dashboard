c93419cb26844457d7819d3d6f04f14c1fc7be52
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-02-11 18:47:37 +0200

    Create index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+export { default as Calendar } from "./Calendar";
+export { default as ColorPicker } from "./ColorPicker";
+export { default as Customers } from "./Customers";
+export { default as Ecommerce } from "./Ecommerce";
+export { default as Editor } from "./Editor";
+export { default as Kanban } from "./Kanban";
+export { default as Orders } from "./Orders";
+export { default as Area } from "./Charts/Area";
+export { default as Bar } from "./Charts/Bar";
+export { default as ColorMapping } from "./Charts/ColorMapping";
+export { default as Financial } from "./Charts/Financial";
+export { default as Line } from "./Charts/Line";
+export { default as Pyrmind } from "./Charts/Pyrmind";
+export { default as Stacked } from "./Charts/Stacked";
