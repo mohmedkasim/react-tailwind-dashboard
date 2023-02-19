cb3b1d74cfd8c7e258e82754ca5c39ea6ed78311
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-02-19 18:14:36 +0200

    add mt-8 className

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -83,7 +83,7 @@ const Ecommerce = () => {
           </div>
           <div className="mt-10 flex gap-10 flex-wrap justify-center">
             <div className="border-r-1 border-color m-4 pr-10">
-              <div>
+              <div className="mt-8">
                 <p>
                   <span className="text-3xl font-semibold">$93,438</span>
                   <span className="p-1.5 hover:cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
