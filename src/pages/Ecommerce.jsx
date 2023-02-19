06e1f605e34581ce9981d1abe8fc29cd72cfa3ab
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-02-19 18:46:46 +0200

    refactor

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -17,15 +17,23 @@ import {
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Ecommerce = () => {
+
   return (
     <div className="mt-24">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
               <p className="text-2xl">$63,448.78</p>
             </div>
+            <button
+              type="button"
+              style={{ backgroundColor: "blue" }}
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+            >
+              <BsCurrencyDollar />
+            </button>
           </div>
           <div className="mt-6">
             <Button
@@ -33,7 +41,6 @@ const Ecommerce = () => {
               bgColor="blue"
               text="Download"
               borderRadius="10px"
-              size="md"
             />
           </div>
         </div>
@@ -41,29 +48,29 @@ const Ecommerce = () => {
           {earningData.map((item) => (
             <div
               key={item.title}
-              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-4 md:w-56 pt-9 rounded-2xl"
+              className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl "
             >
               <button
                 type="button"
                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
               >
                 {item.icon}
               </button>
               <p className="mt-3">
                 <span className="text-lg font-semibold">{item.amount}</span>
-                {/* bug switch auto color in tailwind: solution -> change it to static variable color as color you want in file and return it dynamic */}
                 <span className={`text-sm text-${item.pcColor} ml-2`}>
                   {item.percentage}
                 </span>
               </p>
-              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+              <p className="text-sm text-gray-400  mt-1">{item.title}</p>
             </div>
           ))}
         </div>
       </div>
+
       <div className="flex gap-10 flex-wrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
           <div className="flex justify-between">
             <p className="font-semibold text-xl">Revenue Updates</p>
             <div className="flex items-center gap-4">
@@ -82,26 +89,26 @@ const Ecommerce = () => {
             </div>
           </div>
           <div className="mt-10 flex gap-10 flex-wrap justify-center">
-            <div className="border-r-1 border-color m-4 pr-10">
-              <div className="mt-8">
+            <div className=" border-r-1 border-color m-4 pr-10">
+              <div>
                 <p>
                   <span className="text-3xl font-semibold">$93,438</span>
-                  <span className="p-1.5 hover:cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
                     23%
                   </span>
                 </p>
                 <p className="text-gray-500 mt-1">Budget</p>
               </div>
               <div className="mt-8">
-                <p>
-                  <span className="text-3xl font-semibold">$48,438</span>
-                </p>
+                <p className="text-3xl font-semibold">$48,487</p>
+
                 <p className="text-gray-500 mt-1">Expense</p>
               </div>
+
               <div className="mt-5">
                 <SparkLine
                   currentColor="blue"
-                  id="line-sparkline"
+                  id="line-sparkLine"
                   type="Line"
                   height="80px"
                   width="250px"
