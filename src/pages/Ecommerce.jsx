564b6365a57fa4b6f9ba6f0cbadbd5f037acc1c6
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-02-19 18:37:45 +0200

    apply stacked to work

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -118,7 +118,9 @@ const Ecommerce = () => {
                 />
               </div>
             </div>
-            <div><Stacked width="320px" height="360px"/></div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
           </div>
         </div>
       </div>
